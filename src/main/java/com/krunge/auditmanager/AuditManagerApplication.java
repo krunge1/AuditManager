be6b2e7153beaa4452e167e9fe42f1e86ab679f9
Kyle Runge <krunge1@hotmail.com>
2023-07-04 11:33:09 -0500

    Initial project setup

---
+++ src/main/java/com/krunge/auditmanager/AuditManagerApplication.java
@@ -0,0 +1,13 @@
+package com.krunge.auditmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krunge/auditmanager/AuditManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krunge.auditmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
